2a90089c67ae7267d3310abfd93f22965485601a
jiyou228 <jhjy504@naver.com>
2025-05-18 15:54:44 +0900

    리스트 UI 수정

--- components/list/SearchList.tsx
+++ components/list/SearchList.tsx
@@ -1,77 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-
-// export default function SearchList({
-//   params,
-// }: {
-//   params: { keyword: string };
-// }) {
-//   const [title, setTitle] = useState("");
-//   const [location, setLocation] = useState("");
-//   const [startDate, setStartDate] = useState("");
-//   const [endDate, setEndDate] = useState("");
-//   const [poster, setPoster] = useState("");
-//   const keyword = decodeURIComponent(params.keyword);
-
-//   return (
-//     <div className="w-full h-screen flex flex-col justify-center items-center">
-//       <div className="flex flex-row justify-start items-end mt-22 lg:mt-23 2xl:mt-24">
-//         <h1 className="font-bold text-2xl" style={{ color: "#026DFF" }}>
-//           {keyword}
-//         </h1>
-//         <p className="font-bold text-xl ml-2"> 검색 결과</p>
-//       </div>
-//       <div className="flex flex-col items-center mt-8">
-//         <div className="grid grid-cols-4 items-center justify-center border-t border-gray-300 p-3 w-full">
-//           <div className="flex justify-center">
-//             <img src="/images/seoul.jpeg" className="w-25 h-35 mx-5" />
-//           </div>
-//           <h1 className="font-bold text-xl text-center w-full">
-//             제 17회 서울재즈페스티벌 2025
-//           </h1>
-//           <p className="text-md text-center w-full">2025.05.30 - 2025.06.01</p>
-//           <p className="text-md text-center w-full">올림픽공원</p>
-//         </div>
-
-//         <div className="grid grid-cols-4 items-center justify-center border-t border-gray-300 p-3 w-full">
-//           <div className="flex justify-center">
-//             <img src="/images/seoul.jpeg" className="w-25 h-35 mx-5" />
-//           </div>
-//           <h1 className="font-bold text-xl text-center w-full">
-//             제 17회 서울재즈페스티벌 2024
-//           </h1>
-//           <p className="text-md text-center w-full">2024.05.30 - 2024.06.01</p>
-//           <p className="text-md text-center w-full">올림픽공원</p>
-//         </div>
-
-//         <div className="grid grid-cols-4 items-center justify-center border-t border-gray-300 p-3 w-full">
-//           <div className="flex justify-center">
-//             <img src="/images/seoul.jpeg" className="w-25 h-35 mx-5" />
-//           </div>
-//           <h1 className="font-bold text-xl text-center w-full">
-//             제 17회 서울재즈페스티벌 2023
-//           </h1>
-//           <p className="text-md text-center w-full">2023.05.30 - 2023.06.01</p>
-//           <p className="text-md text-center w-full">올림픽공원</p>
-//         </div>
-
-//         <div className="grid grid-cols-4 items-center justify-center border-t border-gray-300 p-3 w-full">
-//           <div className="flex justify-center">
-//             <img src="/images/glow.png" className="w-25 h-35 mx-5" />
-//           </div>
-//           <h1 className="font-bold text-xl text-center w-full">
-//             글로우 페스티벌 2025
-//           </h1>
-//           <p className="text-md text-center w-full">2025.05.30 - 2025.06.01</p>
-//           <p className="text-md text-center w-full">주경기장</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-/* 여기서부터 시작 */
 "use client";
 
 import Link from "next/link";
@@ -132,7 +58,7 @@ export default function SearchList({
   });
 
   return (
-    <div className="px-50 mt-4">
+    <div className="px-50 mt-72">
       <div className="flex flex-row justify-between">
         <div className="mb-4 flex">
           <h1 className="font-semibold text-2xl text-blue-600">
@@ -152,28 +78,33 @@ export default function SearchList({
           </select>
         </div>
       </div>
+
       {sortedResults.length === 0 ? (
-        <p className="text-xl ">검색 결과가 없습니다.</p>
+        <p className="text-xl">검색 결과가 없습니다.</p>
       ) : (
         sortedResults.map((item, idx) => (
           <Link
             key={idx}
             href={`http://localhost:3000/ticket/${item.performanceId}`}
-            className="block"
+            className="block border-t border-gray-300 w-full"
           >
-            <div className="flex flex-row mb-4 p-6 border rounded justify-between items-center hover:bg-gray-100 cursor-pointer transition">
-              <img
-                src={`http://localhost:8080/performance_poster/${
-                  item.posterUrl.split("/performance_poster")[1]
-                }`}
-                alt="poster"
-              />
-              <h2 className="text-xl font-bold">{item.title}</h2>
-
-              <p>
-                {item.startDate} ~ {item.endDate}
+            <div className="grid grid-cols-4 items-center justify-center p-3 w-full hover:bg-gray-100 cursor-pointer transition">
+              <div className="flex justify-center">
+                <img
+                  src={`http://localhost:8080/performance_poster/${
+                    item.posterUrl.split("/performance_poster")[1]
+                  }`}
+                  alt="poster"
+                  className="w-25 h-35 mx-5"
+                />
+              </div>
+              <h1 className="font-bold text-xl text-center w-full">
+                {item.title}
+              </h1>
+              <p className="text-md text-center w-full">
+                {item.startDate} - {item.endDate}
               </p>
-              <p>{item.location}</p>
+              <p className="text-md text-center w-full">{item.location}</p>
             </div>
           </Link>
         ))
